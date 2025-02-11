6c6847a32efa34cb0b8fb365c951e4565adfaadb
MaitrikKaushik <maitrik2006.kaushik.morni@gmail.com>
2025-02-11 10:45:07 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,7 +3,7 @@
 int main(){
     int side1,side2,side3;
     scanf("%d %d %d",&side1,&side2,&side3);
-    if((side1+side3>side2)||(side1+side2>side3)||(side3+side2>side1)){
+    if((side1+side3>side2)&&(side1+side2>side3)&&(side3+side2>side1)){
         printf("Valid");
         return 0;
     }
