dffef667dc9e4eb4c6aca9d47f91b255de4af2df
MaitrikKaushik <maitrik2006.kaushik.morni@gmail.com>
2025-02-13 09:32:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,21 +3,22 @@ int main(){
     int num1,num2;
     char sign;
     scanf("%d %d %c",&num1,&num2,&sign);
+    int sum,sub,mul,divide;
     switch(sign){
         case '+':
-        int sum = num1 + num2;
+        sum = num1 + num2;
         printf("%d",sum);
         break;
         case '-':
-        int sub = num1 - num2;
+        sub = num1 - num2;
         printf("%d",sub);
         break;
         case '*':
-        int mul = num1 * num2;
+        mul = num1 * num2;
         printf("%d",mul);
         break;
         case '/':
-        int divide = num1 / num2;
+        divide = num1 / num2;
         printf("%d",divide);
         break;
     }
